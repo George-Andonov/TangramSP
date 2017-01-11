f49fc09341d30555b71fa0f403cb01f216053861
George Andonov <george.andonov@gmail.com>
2017-01-11 14:32:36 +0200

    Add files via upload

--- js/tangram.js
+++ js/tangram.js
@@ -40,7 +40,17 @@
 		currentTan = document.getElementById("tan1");
 	});
 	
-// -------------- DROPDOWN LIST
+// Upload button
+$(document).on('click', '.browse', function(){
+  var file = $(this).parent().parent().parent().find('.file');
+  file.trigger('click');
+});
+$(document).on('change', '.file', function(){
+  $(this).parent().find('.form-control').val($(this).val().replace(/C:\\fakepath\\/i, ''));
+});
+// END - Upload button
+	
+	// -------------- DROPDOWN LIST
 	
 //Info Silhouette
 	function infoSil() {
