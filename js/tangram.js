09183230a1016c0cfbe81a346e929c194f54c4d1
George Andonov <george.andonov@gmail.com>
2017-02-02 12:00:05 +0200

    Add files via upload

--- js/tangram.js
+++ js/tangram.js
@@ -6,8 +6,6 @@
 	// TweenLite.defaultOverwrite = "none"; // TBD overlaping
 	
 	var indx = 0; 
-	// Random silhouette =Math.floor(Math.random() * (max - min)) + min;
-	// var indx = Math.floor(Math.random() * (silDb.length -1));
 	var slideTime = 2500; // 2.5s for Slideshow
 	var slideRun = "";
 	var sld = 0;
@@ -19,7 +17,8 @@
 	var gridWidth = 2;
 	var gridHeight = 2;
 	var lSnap = true;
-	var currentTan = 1;
+	var currentTan = "tan1";
+	var prevSelTan = "tan1";
 	var curX, curY, curA;
 	// set default movement - Move
 	Draggable.create("#tan1,#tan2,#tan3,#tan4,#tan5,#tan6,#tan7,#tan7f", {type:"x,y", edgeResistance:0.65, bounds:"#grid", throwProps:true, liveSnap:lSnap,
@@ -40,7 +39,7 @@
 		$('#hideBtn').hide();
 		currentTan = document.getElementById("tan1");
 	});
-	
+
 // Upload button
 $(document).on('click', '.browse', function(){
   var file = $(this).parent().parent().parent().find('.file');
@@ -80,7 +79,6 @@ $(document).on('change', '.file', function(){
 			silString = silString +
 			document.getElementById("tan7f")._gsTransform.x +", " +document.getElementById("tan7f")._gsTransform.y +", "+t7Rot + ", " + flip;
 		}
-		
 		var mymodal = $('#infoSilModal');
 		document.getElementById("sildesc").value = silString;
 		mymodal.modal('show');
@@ -115,55 +113,39 @@ $(document).on('change', '.file', function(){
 
 // Draw silhouette
 	function drawSil() {
+		selectActiveTan("none");
 		// document.getElementById("silName").innerHTML = silDb[indx][1] + ' (' + silDb[indx][2] + ')';
+		document.getElementById("visualControlIcon").innerHTML='';
 		document.getElementById("silNo").value = silDb[indx][0];
 		document.getElementById("groupMenu").innerHTML= silDb[indx][2] + ' <span class="caret caret-down"></span>';
 		document.getElementById("silMenu").innerHTML= silDb[indx][1] + ' <span class="caret caret-down"></span>';
-				
+		
 		TweenLite.to(document.getElementById("tan1"), dur, {x:silDb[indx][3], y:silDb[indx][4], rotation:silDb[indx][5]});
 		TweenLite.to(document.getElementById("tan2"), dur, {x:silDb[indx][6], y:silDb[indx][7], rotation:silDb[indx][8]});
 		TweenLite.to(document.getElementById("tan3"), dur, {x:silDb[indx][9], y:silDb[indx][10], rotation:silDb[indx][11]});
 		TweenLite.to(document.getElementById("tan4"), dur, {x:silDb[indx][12], y:silDb[indx][13], rotation:silDb[indx][14]});
 		TweenLite.to(document.getElementById("tan5"), dur, {x:silDb[indx][15], y:silDb[indx][16], rotation:silDb[indx][17]});
 		TweenLite.to(document.getElementById("tan6"), dur, {x:silDb[indx][18], y:silDb[indx][19], rotation:silDb[indx][20]});
+		TweenLite.to(document.getElementById("tan7"), dur, {x:silDb[indx][21], y:silDb[indx][22], rotation:silDb[indx][23]});
+		TweenLite.to(document.getElementById("tan7f"), dur, {x:silDb[indx][21], y:silDb[indx][22], rotation:silDb[indx][23]});
 		if (silDb[indx][24]==0){
 			flip=0;
 			document.getElementById("tan7").setAttribute("class", "showTan");
 			document.getElementById("tan7f").setAttribute("class", "hidTan");
-			
-			TweenLite.to(document.getElementById("tan7"), dur, {x:silDb[indx][21], y:silDb[indx][22], rotation:silDb[indx][23]});
 		} else {
 			flip=1;
 			document.getElementById("tan7f").setAttribute("class", "showTan");
 			document.getElementById("tan7").setAttribute("class", "hidTan");
-			TweenLite.to(document.getElementById("tan7f"), dur, {x:silDb[indx][21], y:silDb[indx][22], rotation:silDb[indx][23]});
 		}
 	}
 
 // Hide	- Show border
 	function hideBorder() {
-		if ( hideB==0){
-			document.getElementById("silhouette").setAttribute("class", "showSil");
-			if (flip==0){
-				document.getElementById("tan7").setAttribute("class", "showSil");
-			} else {
-				document.getElementById("tan7f").setAttribute("class", "showSil");
-			}
-			hideB=1;
-		}
-		else {
-			document.getElementById("silhouette").setAttribute("class", "hidSil");
-			if (flip==0){
-				document.getElementById("tan7").setAttribute("class", "hidSil");
-			} else {
-				document.getElementById("tan7f").setAttribute("class", "hidSil");
-			}
-			hideB=0;
-		}
+		$("#silhouette").toggleClass("showSil");
 	}
 
 // Navigation	
-// Next sil
+// Next silhouette
 	function nextSil() {
 		if(indx < silDb.length-1) {
 			indx++;
@@ -173,7 +155,7 @@ $(document).on('change', '.file', function(){
 		}
 		drawSil();
 	}
-// Previous sil
+// Previous silhouette
 	function prevSil() {
 		if(indx >1 ) {
 			indx--;
@@ -253,29 +235,30 @@ var drawRandSil = function (){
 	}
 	
 // Move - Rotate tans
-		function moveRotate() {
-			if (action==1) {
-				// Move
-				action=0;
-				document.getElementById("actBtn").innerHTML='<i class="glyphicon glyphicon-move"></i>';
-				// Draggable.get("#tan1,#tan2,#tan3,#tan4,#tan5,#tan6,#tan7,#tan7f").disable();
-				Draggable.create("#tan1,#tan2,#tan3,#tan4,#tan5,#tan6,#tan7,#tan7f", {type:"x,y", edgeResistance:0.65, throwProps:true, liveSnap:lSnap,
-					snap: {
-						x: function(endValue) {
-							return Math.round(endValue / gridWidth) * gridWidth;
-						},
-						y: function(endValue) {
-							return Math.round(endValue / gridHeight) * gridHeight;
-						}
-				}});
-			}
-			else {
-				// Rotation
-				action=1;
-				document.getElementById("actBtn").innerHTML='<i class="glyphicon glyphicon-repeat"></i>';
-				Draggable.create("#tan1,#tan2,#tan3,#tan4,#tan5,#tan6,#tan7,#tan7f", {type: "rotation", bounds:{minRotation:0, maxRotation:360}, throwProps: true });
-			}
+	function moveRotate() {
+		if (action==1) {
+			// Move
+			action=0;
+			document.getElementById("actBtn").innerHTML='<i class="glyphicon glyphicon-move"></i>';
+			// Draggable.get("#tan1,#tan2,#tan3,#tan4,#tan5,#tan6,#tan7,#tan7f").disable();
+			Draggable.create("#tan1,#tan2,#tan3,#tan4,#tan5,#tan6,#tan7,#tan7f", {type:"x,y", edgeResistance:0.65, throwProps:true, liveSnap:lSnap,
+				snap: {
+					x: function(endValue) {
+						return Math.round(endValue / gridWidth) * gridWidth;
+					},
+					y: function(endValue) {
+						return Math.round(endValue / gridHeight) * gridHeight;
+					}
+			}});
 		}
+		else {
+			// Rotation
+			action=1;
+			document.getElementById("actBtn").innerHTML='<i class="glyphicon glyphicon-repeat"></i>';
+			Draggable.create("#tan1,#tan2,#tan3,#tan4,#tan5,#tan6,#tan7,#tan7f", {type: "rotation", bounds:{minRotation:0, maxRotation:360}, throwProps: true });
+		}
+		printXYA();
+	}
 
 		
 // Settings - modal dialogue
@@ -286,29 +269,40 @@ function settingsDialog (){
 
 // Flip romboid
 function flipRomboid() {
-	if (flip==0) {
-		// hide tan7, show tan7f
-		flip=1;
-		document.getElementById("tan7f").setAttribute("class", "showTan");
-		document.getElementById("tan7").setAttribute("class", "hidTan");
-	} else{
-		// hide tan7f, show tan7
-		flip=0;
-		document.getElementById("tan7").setAttribute("class", "showTan");
-		document.getElementById("tan7f").setAttribute("class", "hidTan");
+	if (document.getElementById("cbGameMode").checked){
+		if(prevSelTan=="tan7"||prevSelTan=="tan7f"){
+			prevSelTan="none";
+		}
+		if (flip==0) {
+			// Show tan7f, hide tan7
+			flip=1;
+			//currentTan = document.getElementById("tan7f");
+			document.getElementById("tan7f").setAttribute("class", "showTan");
+			document.getElementById("tan7").setAttribute("class", "hidTan");
+			selectActiveTan("tan7f");
+		} else{
+			// Show tan7, hide tan7f
+			flip=0;
+			//currentTan = document.getElementById("tan7");
+			document.getElementById("tan7").setAttribute("class", "showTan");
+			document.getElementById("tan7f").setAttribute("class", "hidTan");
+			selectActiveTan("tan7");
+		}
 	}
 }
 		
 // Settings modal dialogue
 function gameMode() {
-	var x = document.getElementById("cbGameMode");
-    if(x.checked){
+    if(document.getElementById("cbGameMode").checked){
 		document.getElementById("actBtn").classList.remove("disabled");
 		document.getElementById("actBtn").setAttribute("onclick", "moveRotate()");
 		document.getElementById("flipBtn").classList.remove("disabled");
 		document.getElementById("flipBtn").setAttribute("onclick", "flipRomboid()");
 		$('#actBtn').show();
 		$('#flipBtn').show();
+		$('#coordinates').show();
+		sld=0;
+		slideShow();
 	}
 	else {
 		document.getElementById("actBtn").className += " disabled";
@@ -317,13 +311,16 @@ function gameMode() {
 		document.getElementById("flipBtn").removeAttribute('onclick');
 		$('#actBtn').hide();
 		$('#flipBtn').hide();
+		$('#coordinates').hide();
+		currentTan.classList.toggle("showTan");
 	}
+	currentTan.classList.toggle("selectedTan");
+	printXYA();
 }
 
 // Show shadow
 function showShadow() {
-	var x = document.getElementById("cbShadow");
-    if(x.checked){
+    if(document.getElementById("cbShadow").checked){
 		document.getElementById("silhouette").setAttribute("filter", "url(#f3)");
 	}
 	else {
@@ -332,8 +329,7 @@ function showShadow() {
 }
 
 function showHint() {
-    var x = document.getElementById("cbHint");
-    if(x.checked){
+    if(document.getElementById("cbHint").checked){
 		document.getElementById("hideBtn").classList.remove("disabled");
 		document.getElementById("hideBtn").setAttribute("onclick", "hideBorder()");
 		$('#hideBtn').show();
@@ -349,16 +345,53 @@ function showHint() {
 
 // Print coordinates
 function printXYA() {
-	document.getElementById("silX").value = Math.round(currentTan._gsTransform.x);
-	document.getElementById("silY").value = Math.round(currentTan._gsTransform.y);
-	document.getElementById("silA").value = Math.round(currentTan._gsTransform.rotation);
+	if (document.getElementById("cbGameMode").checked){
+		curX = Math.round(currentTan._gsTransform.x);
+		curY = Math.round(currentTan._gsTransform.y);
+		document.getElementById("silX").value = curX;
+		document.getElementById("silY").value = curY;
+		document.getElementById("silA").value = Math.round(currentTan._gsTransform.rotation);
+		document.getElementById("visualControlIcon").setAttribute("x", curX);
+		document.getElementById("visualControlIcon").setAttribute("y", curY);
+		if (action==0){
+			document.getElementById("visualControlIcon").innerHTML='&#xf047;';
+		}
+		else {
+			document.getElementById("visualControlIcon").innerHTML='&#xf01e;';
+		}
+	}
+	else{
+		document.getElementById("visualControlIcon").innerHTML='';
+	}
+}
+
+// Select Tan
+function selectActiveTan(tan){
+	if (document.getElementById("cbGameMode").checked){
+		if (tan!="none"){
+			if(prevSelTan!="none"){
+				currentTan.classList.toggle("selectedTan");
+			}
+			currentTan = document.getElementById(tan);
+			currentTan.classList.toggle("selectedTan");
+			// print X, Y, A coordinates
+			document.getElementById("curS").value = tan;
+			printXYA();
+		}
+		else{
+			if(prevSelTan!="none"){
+				currentTan.classList.toggle("selectedTan");
+			}
+		}
+		prevSelTan = tan;
+	}
 }
 
 // Set Current Tan's coordinates
 function setTan() {
-	curX = Math.round(currentTan._gsTransform.x);
-	curY = Math.round(currentTan._gsTransform.y);
-	curA = Math.round(currentTan._gsTransform.rotation);
+	curX = document.getElementById("silX").value;
+	curY = document.getElementById("silY").value;
+	curA = document.getElementById("silA").value;
 	TweenLite.to(currentTan, 0.5, {x:curX});
 	TweenLite.to(currentTan, 0.5, {y:curY});
 	TweenLite.to(currentTan, 0.5, {rotation:curA});
@@ -420,65 +453,46 @@ function checkKeys(e) {
 			TweenLite.to(currentTan, 0.5, {rotation:curA});
 			printXYA();
             break;
+		case 96:
+            // Numpad - 0 key pressed
+			selectActiveTan("none");
+            break;
 		case 97:
             // Numpad - 1 key pressed
-			currentTan = document.getElementById("tan1");
-			// print X, Y, A coordinates
-			document.getElementById("curS").value = "1";
-			printXYA();
+			selectActiveTan("tan1");
             break;
 		case 98:
             // Numpad - 2 pressed
-			currentTan = document.getElementById("tan2");
-			document.getElementById("curS").value = "2";
-			printXYA();
+			selectActiveTan("tan2");
             break;
 		case 99:
             // Numpad - 3 key pressed
-			currentTan = document.getElementById("tan3");
-			document.getElementById("curS").value = "3";
-			printXYA();
+			selectActiveTan("tan3");
             break;
 		case 100:
             // Numpad - 4 pressed
-			currentTan = document.getElementById("tan4");
-			document.getElementById("curS").value = "4";
-			printXYA();
+			selectActiveTan("tan4");
             break;
 		case 101:
             // Numpad - 5 key pressed
-			currentTan = document.getElementById("tan5");
-			document.getElementById("curS").value = "5";
-			printXYA();
+			selectActiveTan("tan5");
             break;
 		case 102:
             // Numpad - 6 pressed
-			currentTan = document.getElementById("tan6");
-			document.getElementById("curS").value = "6";
-			printXYA();
+			selectActiveTan("tan6");
             break;
 		case 103:
             // Numpad - 7 key pressed
 			if (flip==0){
-				currentTan = document.getElementById("tan7");
+				selectActiveTan("tan7");
 			}
 			else {
-				currentTan = document.getElementById("tan7f");
+				selectActiveTan('tan7f');
 			}
-			document.getElementById("curS").value = "7";
-			printXYA();
             break;
 		case 191:
             // question key pressed
 			hideBorder();
-            break; 
-		case 107:
-            // + key pressed
-			$('#coordinates').show();
-            break; 
-		case 109:
-            // - key pressed
-			$('#coordinates').hide();
             break; 
     }   
 }
